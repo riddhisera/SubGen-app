6907de1ceb1a6e65b1ed5dd9108fb3e11af7a732
Riddhi Sera <sera.1@iitj.ac.in>
2021-07-29 18:07:04 +0530

    added 3 steps

--- first_app.py
+++ first_app.py
@@ -2,22 +2,38 @@ import streamlit as st
 import os
 import base64
 import pandas as pd
+import io
 
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    data = [(1, 2, 3)]
-    # When no file name is given, pandas returns the CSV as a string, nice.
-    df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
-    csv = df.to_csv(index=False)
+##################### FUNCTIONS #####################
 
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
-    return href
+def download_link(object_to_download, download_filename, download_link_text):   # Generates a link to download the given object_to_download.
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
+def splitter(input_file, no_seq=500):   # to split files into 50MB    
+    mylist = input_file.split(">")[1:]
+    n = len(mylist)//no_seq + 2
+    x = 0
+    s = ''
 
+    newlist = []
+    for i in range(1,n):
+        for text in mylist[x:(i)*no_seq]:
+            s += ">"+text
+        x = i*no_seq 
+        newlist.append(s)
+        s = ''
+    return newlist
+
+def combiner(input_files): # to combine files for blast
+    output = ''
+    for file in input_files:
+        stringio = io.StringIO(file.getvalue().decode("utf-8"))
+        string_data = stringio.read()
+        output += string_data + '\n'
+    return output
+
+##################### MAIN TEXT ###################################
 
 st.title('Drug Discovery using Subtractive Genomics Analysis')
 "Find `drug targets` for your organism using `Subtractive Genomics Approch`"
@@ -39,18 +55,37 @@ st.subheader('How do I use this tool?')
 "`STEP 1:` Collect the proteome data from the NCBI-protein database into a single fasta file."
 st.info('If your target organism is Escherichia coli, go to https://www.ncbi.nlm.nih.gov/protein/?term=Escherichia+coli and download the Fasta file for all the results using the "Send to:" option')
 "`STEP 2:` Perform clustering using CD-HIT"
-st.info('Go to: http://weizhong-lab.ucsd.edu/cdhit_suite/cgi-bin/index.cgi?cmd=cd-hit and load the Fasta file from step one. Set the sequence identity cut-off to 0.8 and leave the rest of the parameters to default. Give your mail address and click Submit!')
+st.info('Go to: http://weizhong-lab.ucsd.edu/cdhit_suite/cgi-bin/index.cgi?cmd=cd-hit and load the Fasta file from step one. Set the sequence identity cut-off to 0.8 and leave the rest of the parameters to its default values. Give your mail address and click Submit!')
+"`STEP 3:` Perform Local blast with Human & Mouse protein data"
+st.info('Click here to get the combined Human & Mouse data you need to perform blast [insert link]. Perform local blast on your computer.')
 
-# Sidebar
+
+######################### Sidebar ######################
+
+st.sidebar.title('Tools to help you!')
+
 with st.sidebar.header('File splitter (for step 2)'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['Fasta'])
-    load_data = pd.read_table(uploaded_file, sep=' ', header=None)
-    
+    uploaded_file = st.sidebar.file_uploader("To split files into smaller pieces of 50MB as required by the CD-HIT suite", type=['Fasta','txt'])
+
+int_val = st.sidebar.number_input('Specify how many sequences you want in a file, the default is set to 500', min_value=1, max_value=1000, value=500, step=100)
+
 if st.sidebar.button('Split'):
-    # split the uploaded_file
-    st.sidebar.info('Done! collect your files from below by right clicking the link & save as')
-    st.sidebar.markdown(get_table_download_link(load_data), unsafe_allow_html=True)
+    stringio = io.StringIO(uploaded_file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+
+    for s, i in zip(splitter(string_data, int_val),range(1,int_val+1)):
+        tmp_download_link = download_link(s, 'file'+str(i)+'.txt', 'download file '+str(i))
+        st.sidebar.markdown(tmp_download_link, unsafe_allow_html=True)
 else:
-    st.sidebar.write("[Example input file](https://raw.githubusercontent.com/dataprofessor/bioactivity-prediction-app/main/example_acetylcholinesterase.txt)")
-    st.sidebar.info('Upload input data in the sidebar to start!')
+    st.sidebar.write('`Upload input data in the sidebar to get started!`')
+
+
+with st.sidebar.header('File combiner (for step 3)'):
+    uploaded_files = st.sidebar.file_uploader("To combine the split files from CD HIT to perform Blast", type=['Fasta','txt'], accept_multiple_files=True)
+
+if st.sidebar.button('Combine'):
+    tmp_download_link = download_link(combiner(uploaded_files), 'combinedfile.txt', 'download combined file')
+    st.sidebar.markdown(tmp_download_link, unsafe_allow_html=True)
+else:
+    st.sidebar.write('`Upload input data in the sidebar to get started!`')
 
